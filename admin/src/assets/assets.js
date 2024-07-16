ba2e023b47ab241ce6fbf5c2f9bb0cf1a44300c4
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2024-07-16 15:33:43 +0500

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,25 +1,17 @@
-import logo from "./logo.png";
-import logo_small from "./logo_small.png";
-import upload_area from "./upload_area.png";
+import uploadImg from "./uploadImg.png";
+import uploadSong from "./uploadSong.png";
 import upload_added from "./upload_added.png";
-import add_album from "./add_album.png";
-import add_song from "./add_song.png";
-import song_icon from "./song_icon.png";
-import album_icon from "./album_icon.png";
-import upload_song from "./upload_song.png";
 import user from "./peo1.png";
 import remove from "./remove.png";
+import logo from "./beatflow.png";
+import logoMini from "./beatflowMini.png";
 
 export const assets = {
+  uploadImg,
+  uploadSong,
+  logoMini,
   remove,
   user,
   logo,
-  logo_small,
-  upload_area,
   upload_added,
-  add_album,
-  add_song,
-  song_icon,
-  album_icon,
-  upload_song,
 };
